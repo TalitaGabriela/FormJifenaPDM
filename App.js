845f2f1b3482d0db7d0fe476edb88667c171faad
TalitaGabriela <talitagabriela1216@gmail.com>
2024-03-22 14:15:05 -0400

    Commit

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+
+function Header() {
+  return(
+    <View style={styles.header}>
+      <Text style={styles.text}>
+        JIFENA 2024 - Ficha de Cadastro:
+      </Text>
+    </View>
+  )
+}
+
+function Footer() {
+  return(
+    <View style={styles.footer}>
+      <Text style={styles.text}>
+      PDM1 © 2024
+      </Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <Text>Nome:</Text>
+      <TextInput style={styles.input}></TextInput>
+      <Text>Sobrenome:</Text>
+      <TextInput style={styles.input}></TextInput>
+      <Text>Sexo:</Text>
+      <TextInput style={styles.input}></TextInput>
+      <Text>Idade:</Text>
+      <TextInput style={styles.input}></TextInput>
+      <Text>
+      {'\n'}
+      </Text>
+      <Pressable style={styles.submit}> ENVIAR </Pressable>
+
+      <Footer/>
     </View>
   );
 }
@@ -16,5 +50,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: "37px",
+    color: "white",
+    textAlign: "center",
+  },
+  header: {
+    display: "flex",
+    position: 'fixed',
+    top: 0,
+    backgroundColor: "green",
+    width: "100%",
+  },
+  footer: {
+    display: 'flex',
+    position: 'fixed',
+    bottom: 0,
+    backgroundColor: 'green',
+    width: "100%",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  submit:{
+    fontFamily: "Arial",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 3,
+    backgroundColor: "#d3d3d3",
+    alignItems: "center",
+    width: "162px",
   },
 });
